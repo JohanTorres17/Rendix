39200a38c26dad906a546b5a77cc0627d0384e0d
Johan <johan.torress@uniagustiniana.edu.co>
2026-03-01 23:24:22 -0500

    correcciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from './components/Navbar.jsx'
+import Navbar from './components/NavBar.jsx'
 import Cover from './components/Cover.jsx'
 import LaMarca from './components/LaMarca.jsx'
 import Concepto from './components/Concepto.jsx'
